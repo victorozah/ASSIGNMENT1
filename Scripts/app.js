1471679dcf421de76fa8cffa1f99fd7ebf55a89a
victorozah <vozah@my.centennialcollege.ca>
2023-01-15 07:05:14 -0500

    init

---
+++ Scripts/app.js
@@ -0,0 +1,21 @@
+/*myVariable = 25;
+console.log("My number is " + myVariable);*/
+
+/*let name = 'Victor';
+console.log(name);*/
+
+/*let person = {
+    name: 'Victor',
+    age: 25
+};
+console.log(person);*/
+
+/*function greet(name){
+    console.log('Hello '+ name);
+}
+greet('John')*/
+
+function square(number){
+    return number * number;
+}
+console.log(square(2));
